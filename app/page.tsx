f468f214970be5ac010a74a9e6022e3b85ea71cb
impruthvi <pruthvirajput97@gmail.com>
2024-09-02 16:05:34 +0530

    rendered mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Intro from "@/components/intro"
+import { MDXRemote } from "next-mdx-remote/rsc"
 
 const Home = () => {
+
+  const content = `
+    # Hello World
+  `
+
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
           <Intro />
+          <MDXRemote source={content} />
       </div>
     </section>
   )
---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,69 @@
+import { getPostBySlug } from '@/actions/posts'
+import { formatDate } from '@/lib/utils'
+import { ArrowLeftIcon } from 'lucide-react'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import Image from 'next/image'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+const Post = async ({ params: { slug } }: Props) => {
+  const post = await getPostBySlug(slug)
+
+  if (!post) {
+    notFound()
+  }
+
+  const { metadata, content } = post
+  const { title, image, author, publishedAt } = metadata
+
+  const component = {
+    h2: (props: any) => <h2 {...props} className='text-red-500'>{props.children}</h2>,
+  }
+
+  return (
+    <section className='pb-24 pt-32'>
+      <div className='container max-w-3xl'>
+        <Link
+          href='/posts'
+          className='mb-8 inline-flex items-center gap-2 text-sm font-light text-muted-foreground transition-colors hover:text-foreground'
+        >
+          <ArrowLeftIcon className='h-5 w-5' />
+          <span>Back to posts</span>
+        </Link>
+
+        {image && (
+          <div className='relative mb-6 h-96 w-full overflow-hidden rounded-lg'>
+            <Image
+              src={image}
+              alt={title || ''}
+              className='object-cover'
+              fill
+            />
+          </div>
+        )}
+
+        <header>
+          <h1 className='title'>{title}</h1>
+          <p className='mt-3 text-xs text-muted-foreground'>
+            {author} / {formatDate(publishedAt ?? '')}
+          </p>
+        </header>
+
+        <main className='prose dark:prose-invert mt-16'>
+          <MDXRemote source={content} components={component}/>
+        </main>
+
+        <footer className='mt-16'></footer>
+      </div>
+    </section>
+  )
+}
+
+export default Post
