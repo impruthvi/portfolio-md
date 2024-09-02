d351c71ea243467963dfa1e7a7a1f79172c35fab
impruthvi <pruthvirajput97@gmail.com>
2024-09-02 16:54:18 +0530

    added posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,11 +1,20 @@
+import { getPosts } from '@/actions/posts'
+import Posts from '@/components/posts'
 import React from 'react'
 
-type Props = {
-    
+type Props = {}
+
+const PostsPage = async (props: Props) => {
+  const posts = await getPosts()
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Posts</h1>
+
+        <Posts posts={posts} />
+      </div>
+    </section>
+  )
 }
 
-const Posts = (props: Props) => {
-  return <div>Posts</div>
-}
-
-export default Posts
+export default PostsPage
