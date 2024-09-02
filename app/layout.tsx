478078b85aeb0e16c50e4de40ea53a02dbc937a7
impruthvi <pruthvirajput97@gmail.com>
2024-09-02 14:56:08 +0530

    added header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Providers from './providers/provider'
+import { Inter, Playfair_Display } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from '@/lib/utils'
+
+import './globals.css'
+import Providers from '@/providers'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
+      <body
+        className={cn(
+          'flex min-h-screen flex-col font-sans antialiased',
+          inter.variable,
+          playfair.variable
+        )}
+      >
+        <Providers>
+          <Header />
+          <main className='grow'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
