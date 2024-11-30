93a549fd985dbad13e89c934ec491b72a533cd24
impruthvi <pruthvirajput97@gmail.com>
2024-11-30 15:55:20 +0530

    feat: update introduction text to enhance clarity and highlight additional skills

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,9 +7,11 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hey, I&#39;m Pruthvisinh Rajput.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a passionate Software Developer 🚀 having an experience of
-          building Web with Nextjs / Nodejs / Reactjs / Laravel / PHP and some
-          other cool libraries and frameworks.
+          I am a dedicated Software Developer with expertise in building web
+          applications using Next.js, Node.js, React.js, Laravel, PHP, and a
+          range of other modern libraries and frameworks. Additionally, I
+          possess strong knowledge of AWS and server management, enabling me to
+          deliver scalable and efficient solutions.
         </p>
       </div>
       <div className='relative'>
