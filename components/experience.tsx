de6812cb822b97e71021e25ee16cf4f127b98be7
impruthvi <pruthvirajput97@gmail.com>
2025-09-20 00:08:41 +0530

    feat: update experience section with new role at Vivansh Infotech and correct date for Yudiz Solution

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,9 +1,16 @@
 export default function Experience() {
   const experience = [
     {
+      company: 'Vivansh Infotech',
+      position: 'Software Engineer',
+      date: 'Jun 2025 – Present',
+      description:
+        'As a Software Engineer at Vivansh Infotech, I focus on backend development while actively contributing to frontend projects. I thrive in dynamic environments, enjoy exploring new cultures, and am always eager to learn and grow professionally.'
+    },
+    {
       company: 'Yudiz Solution',
       position: 'Back-End Developer',
-      date: 'Jan 2022 – Present',
+      date: 'Jan 2022 – May 2025',
       description:
         'As a Web Developer at Yudiz Solutions Ltd, a leading web and mobile app development company, I specialize in backend development and hands-on involvement in frontend tasks. Eager to explore new cultures and environments, I am consistently energetic and driven to acquire new skills.'
     },
